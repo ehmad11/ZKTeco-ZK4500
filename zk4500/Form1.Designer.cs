fd8ccb745f67b28ecb0472abfc665b047b7ab85f
ehmad11 <ehmad1@gmail.com>
2018-06-05 10:34:06 +0500

    zk4500

---
+++ zk4500/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace zk4500
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.prompt = new System.Windows.Forms.Label();
+            this.fpicture = new System.Windows.Forms.PictureBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.deviceSerial = new System.Windows.Forms.Label();
+            this.txtTemplate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fpicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Enabled = false;
+            this.btnVerify.Location = new System.Drawing.Point(569, 68);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(95, 38);
+            this.btnVerify.TabIndex = 2;
+            this.btnVerify.Text = "&Verify";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // prompt
+            // 
+            this.prompt.AutoSize = true;
+            this.prompt.Location = new System.Drawing.Point(139, 352);
+            this.prompt.Name = "prompt";
+            this.prompt.Size = new System.Drawing.Size(50, 13);
+            this.prompt.TabIndex = 3;
+            this.prompt.Text = "Ready ...";
+            // 
+            // fpicture
+            // 
+            this.fpicture.BackColor = System.Drawing.SystemColors.Window;
+            this.fpicture.Location = new System.Drawing.Point(142, 56);
+            this.fpicture.Name = "fpicture";
+            this.fpicture.Size = new System.Drawing.Size(268, 283);
+            this.fpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.fpicture.TabIndex = 21;
+            this.fpicture.TabStop = false;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(468, 67);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(95, 39);
+            this.btnRegister.TabIndex = 22;
+            this.btnRegister.Text = "&Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // deviceSerial
+            // 
+            this.deviceSerial.AutoSize = true;
+            this.deviceSerial.Location = new System.Drawing.Point(465, 176);
+            this.deviceSerial.Name = "deviceSerial";
+            this.deviceSerial.Size = new System.Drawing.Size(76, 13);
+            this.deviceSerial.TabIndex = 3;
+            this.deviceSerial.Text = "Device Serial: ";
+            // 
+            // txtTemplate
+            // 
+            this.txtTemplate.Location = new System.Drawing.Point(468, 214);
+            this.txtTemplate.Multiline = true;
+            this.txtTemplate.Name = "txtTemplate";
+            this.txtTemplate.Size = new System.Drawing.Size(424, 82);
+            this.txtTemplate.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(465, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Template: ";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(311, 318);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(99, 21);
+            this.btnClear.TabIndex = 22;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 415);
+            this.Controls.Add(this.txtTemplate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.fpicture);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deviceSerial);
+            this.Controls.Add(this.prompt);
+            this.Controls.Add(this.btnVerify);
+            this.Name = "Form1";
+            this.Text = "ZK4500";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fpicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.Label prompt;
+        private System.Windows.Forms.PictureBox fpicture;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label deviceSerial;
+        private System.Windows.Forms.TextBox txtTemplate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ zk4500/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxZKFPEngXControl;
+
+namespace zk4500
+{
+    public partial class Form1 : Form
+    {
+        private AxZKFPEngX ZkFprint = new AxZKFPEngX();
+        private bool Check;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Controls.Add(ZkFprint);
+            InitialAxZkfp();
+        }
+
+        private void InitialAxZkfp()
+        {
+            try
+            {
+
+                ZkFprint.OnImageReceived += zkFprint_OnImageReceived;
+                ZkFprint.OnFeatureInfo += zkFprint_OnFeatureInfo;
+                //zkFprint.OnFingerTouching 
+                //zkFprint.OnFingerLeaving
+                ZkFprint.OnEnroll += zkFprint_OnEnroll;
+
+                if (ZkFprint.InitEngine() == 0)
+                {
+                    ZkFprint.FPEngineVersion = "9";
+                    ZkFprint.EnrollCount = 3;
+                    deviceSerial.Text += " " + ZkFprint.SensorSN + " Count: " + ZkFprint.SensorCount.ToString() + " Index: " + ZkFprint.SensorIndex.ToString();
+                    ShowHintInfo("Device successfully connected");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                ShowHintInfo("Device init err, error: " + ex.Message);
+            }
+        }
+
+    private void zkFprint_OnImageReceived(object sender, IZKFPEngXEvents_OnImageReceivedEvent e)
+        {
+            Graphics g = fpicture.CreateGraphics();
+            Bitmap bmp = new Bitmap(fpicture.Width, fpicture.Height);
+            g = Graphics.FromImage(bmp);
+            int dc = g.GetHdc().ToInt32();
+            ZkFprint.PrintImageAt(dc, 0, 0, bmp.Width, bmp.Height);
+            g.Dispose();
+            fpicture.Image = bmp;
+        }
+
+        private void zkFprint_OnFeatureInfo(object sender, IZKFPEngXEvents_OnFeatureInfoEvent e)
+        {
+
+            String strTemp = string.Empty;
+            if (ZkFprint.EnrollIndex != 1)
+            {
+                if (ZkFprint.IsRegister)
+                {
+                    if (ZkFprint.EnrollIndex - 1 > 0)
+                    {
+                        int eindex = ZkFprint.EnrollIndex - 1;
+                        strTemp = "Please scan again ..." + eindex;
+                    }
+                }
+            }
+            ShowHintInfo(strTemp);
+        }
+        private void zkFprint_OnEnroll(object sender, IZKFPEngXEvents_OnEnrollEvent e)
+        {
+            if (e.actionResult)
+            {
+
+
+                string template = ZkFprint.EncodeTemplate1(e.aTemplate);
+                txtTemplate.Text = template;
+                ShowHintInfo("Registration successful. You can verify now");
+                btnRegister.Enabled = false;
+                btnVerify.Enabled = true;
+            }
+            else
+            {
+                ShowHintInfo("Error, please register again.");
+
+            }
+        }
+        private void zkFprint_OnCapture(object sender, IZKFPEngXEvents_OnCaptureEvent e)
+        {
+            string template = ZkFprint.EncodeTemplate1(e.aTemplate);
+
+
+            if (ZkFprint.VerFingerFromStr(ref template, txtTemplate.Text, false, ref Check))
+            {
+                ShowHintInfo("Verified");
+            }
+            else
+                ShowHintInfo("Not Verified");
+
+        }
+
+
+
+        private void ShowHintInfo(String s)
+        {
+              prompt.Text = s;
+        }
+
+      
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            if (ZkFprint.IsRegister)
+            {
+                ZkFprint.CancelEnroll();
+            }
+            ZkFprint.OnCapture += zkFprint_OnCapture;
+            ZkFprint.BeginCapture();
+            ShowHintInfo("Please give fingerprint sample.");
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+            ZkFprint.CancelEnroll();
+            ZkFprint.EnrollCount = 3;
+            ZkFprint.BeginEnroll();
+            ShowHintInfo("Please give fingerprint sample.");
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            fpicture.Image = null;
+        }
+
+    }
+}
